3261da33e8274ac4eb4e12a1aa8833043c84777e
Gerardo <gerardofingurbe@gmail.com>
2024-04-03 11:14:04 +0200

    little refactoring

--- src/cumulant.jl
+++ src/cumulant.jl
@@ -1,13 +1,7 @@
-
+using IterTools
 using QuadGK
 using QuantumToolbox
-struct cumulant
-    Hsys
-    t::Float64
-    eps::Float64
-    baths
-    Qs
-end
+
 
 struct overdampedBath
     T::Float64
@@ -21,6 +15,84 @@ struct ohmicBath
     wc::Float64
 end
 
+function eigvects_to_kets(vects) #Maybe this is unnecessary
+    return [Qobj(vects[:,i]) for i in 1:size(vects)[1]]
+end
+
+function decays(Hsys,Q,bath,t)
+    ws,jump=jump_operators(Hsys,Q)
+    combinations=collect(product(ws,ws))
+    rates=Dict()
+    for i in eachindex(combinations)
+        j=combinations[i]
+        rates[j]=decay.(j[1],j[2],bath,t) 
+    end
+    return rates
+end
+
+function cum_superop(Hsys,Q)
+    ws,jump=jump_operators(Hsys,Q)
+    combinations=collect(product(ws,ws))
+    superops=Dict()
+    for i in eachindex(combinations)
+        j=combinations[i]
+        superops[j]=spre(jump[j[2]])*spost(jump[j[1]]') - 0.5*(spre(jump[j[1]]' *jump[j[2]])+spost(jump[j[1]]' *jump[j[2]]))
+    end
+    return superops
+end
+
+function generator(Hsys,Q,bath,t)
+    dict=decays(Hsys,Q,bath,t)#fine;
+    dict2=cum_superop(Hsys,Q)#fine;
+    ans=[0.0*Qobj(spre(Q)) for j in 1:length(t) ]
+    for i in collect(keys(dict))
+        ans +=[j *dict2[i] for j in dict[i]]
+    end
+    return ans
+end
+
+function csolve(Hsys,Q,bath,t,rho0)
+    g=generator(Hsys,Q,bath,t)
+    return [Qobj(vec2mat(exp(i).data * mat2vec(rho0.data))) for i in g]
+end
+
+function csolve(Hsys,Q::Array,bath::Array,t,rho0)
+    gens=[]
+    for (x,y) in zip(Q,bath)
+        push!(gens,generator(Hsys,x,y,t))
+    end
+    g=sum(gens)
+    return [Qobj(vec2mat(exp(i).data * mat2vec(rho0.data))) for i in g]
+end
+
+function jump_operators(Hsys,Q)
+    eigvals,eigvects=eigen(Hsys);
+    eigvects=eigvects_to_kets(eigvects);
+    N=length(eigvals);
+    collapse = []
+    ws= []
+    for j=1:N
+        for k=j:N
+            gap=eigvals[k] - eigvals[j]
+            if  !isapprox(gap,0.0) #dephasing
+                push!(ws,gap)
+                push!(collapse,(eigvects[j]*eigvects[j]')*Q*(eigvects[k]*eigvects[k]'))
+                push!(ws,-gap)
+                push!(collapse,eigvects[k]*eigvects[k]'*Q*eigvects[j]*eigvects[j]')
+            end
+        end
+    end
+    push!(collapse,Q-sum(collapse))
+    push!(ws,0)# dephasing
+    for i in eachindex(collapse)
+        if collapse[i]==Qobj(Hsys)*0
+            deleteat!(ws,i)
+            deleteat!(collapse,i)
+        end
+    end
+    jump=Dict(ws .=> collapse)
+    return ws,jump
+end
 
 function bose(Ω,T)
     return 1/(exp(Ω/T)-1)
